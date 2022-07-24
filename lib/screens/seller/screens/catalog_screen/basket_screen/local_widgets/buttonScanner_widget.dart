e30b64e9691b736c13fb730a9cb0535d38e1181e
Kaparovvv <kaparovkanagat@gmail.com>
2022-07-25 01:01:27 +0600

    Scanner

---
+++ lib/screens/seller/screens/catalog_screen/basket_screen/local_widgets/buttonScanner_widget.dart
@@ -0,0 +1,50 @@
+import 'package:cashback_app/commons/box_shadow_helper.dart';
+import 'package:cashback_app/commons/text_style_helper.dart';
+import 'package:cashback_app/commons/theme_helper.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class ButtonScannerWidget extends StatelessWidget {
+  final String buttonTxt;
+  final Widget iconWidget;
+  final Function() function;
+  const ButtonScannerWidget({
+    Key? key,
+    required this.function,
+    required this.buttonTxt,
+    required this.iconWidget,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => function(),
+      child: Container(
+        width: 320.w,
+        height: 40.h,
+        decoration: BoxDecoration(
+          color: ThemeHelper.white,
+          borderRadius: BorderRadius.circular(20.r),
+          boxShadow: [BoxShadowHelper.boxShadow50],
+        ),
+        child: Padding(
+          padding: EdgeInsets.only(left: 60.w, right: 13.w),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              SizedBox(
+                width: 140.w,
+                child: Text(
+                  buttonTxt,
+                  textAlign: TextAlign.center,
+                  style: TextStyleHelper.f14w600green80,
+                ),
+              ),
+              iconWidget
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
