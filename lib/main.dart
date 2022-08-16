720978f423871c9a781087945250629c5b8f767c
Diana Talantbekova <talantbekovadiana71@gmail.com>
2022-08-16 20:49:08 +0600

    Payment Screen's Changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:cashback_app/screens/buyer/buyer_navigation_widget.dart/buyer_navigation_widget.dart';
+import 'package:cashback_app/screens/seller/seller_navigation/seller_navigation_widget.dart';
 import 'package:cashback_app/screens/splash_screen/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -21,8 +22,8 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.blue,
         ),
         home: const 
-        // SplashScreen(),
-        BuyerNavigationWidget(currentIndex: 1),
+        SplashScreen(),
+        // BuyerNavigationWidget(currentIndex: 1),
         // SellerNavigationWidget(currentIndex: 2)
       ),
     );
--- lib/screens/seller/screens/boxOffice_screen.dart/local_widget/titles.dart
+++ lib/screens/seller/screens/boxOffice_screen.dart/local_widget/titles.dart
@@ -1,5 +1,6 @@
 import 'package:cashback_app/commons/text_style_helper.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class Titles extends StatelessWidget {
   const Titles({
@@ -13,7 +14,9 @@ class Titles extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         Text('Наименование', style: TextStyleHelper.f14w600),
+        SizedBox(width: 20.w),
         Text('Стоимость', style: TextStyleHelper.f14w600),
+        SizedBox(width: 20.w),
         Text('Кэшбек', style: TextStyleHelper.f14w600),
       ],
     );
